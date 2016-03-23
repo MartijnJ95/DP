8679a90f4be92c503e91e6f3564e0fdb9a013cdd
KevinVording <kevin.mike.vording@hotmail.com>
2016-03-23 15:10:16 +0100

    Storage
    Storage class voorlopig klaar

--- src/desingPatterns/Storage.java
+++ src/desingPatterns/Storage.java
@@ -1,71 +1,105 @@
 package desingPatterns;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 
-public class Storage {
+public class Storage 
+{
 	
 	// object Lokaal
-	private Lokaal lokaal;
+	private Lokaal Lokaal;
 	// object Gebruiker
-	private Gebruiker gebruiker;
+	private Gebruiker Gebruiker;
+	// object factory
+	private GebruikersFactory gebruikersFactory;
+	// Singleton Instance
+	private static Storage instance = null;
 	
 	// HashMaps
 	private HashMap<String, Lokaal> lokalen = new HashMap<String, Lokaal>();
 	private HashMap<String, Gebruiker> gebruikers = new HashMap<String, Gebruiker>();
 	
 	// Voeg gebruiker toe op naam en object
-	public void AddGebruiker(String gebruikerNaam, Gebruiker gebr) {
+	public void addGebruiker(String gebruikerNaam, Gebruiker gebr) 
+	{
 		gebruikers.put(gebruikerNaam, gebr);
 	}
 	
 	// Voeg lokaal toe op lokaalnummer en object
-	public void AddLokaal(String lokaalNummer, Lokaal lok) {
+	public void addLokaal(String lokaalNummer, Lokaal lok) 
+	{
 		lokalen.put(lokaalNummer, lok);
 	}
 	
 	// Returned de gebruikers list
-	public HashMap<String, Gebruiker> GetAllGebruikers() {
+	public HashMap<String, Gebruiker> getAllGebruikers() 
+	{
 		return gebruikers;
 	}
 	
 	// Returned de lokalen list
-	public HashMap<String, Lokaal> GetAllLokalen() {
+	public HashMap<String, Lokaal> getAllLokalen() 
+	{
 		return lokalen;
 	}
 	
 	// Return de gebruiker op naam
-	public Gebruiker GetGebruikerByName(String name) {
+	public Gebruiker getGebruikerByName(String name) 
+	{
 		return gebruikers.get(name);
 	}
 	
 	
-	public void FillStorage() {
-		Gebruiker kevin = GebruikersFactory.GetGebruiker(Student);
-		Gebruiker martijn = GebruikersFactory.GetGebruiker(Student);
-		Gebruiker wilco = GebruikersFactory.GetGebruiker(Docent);
-		Gebruiker xing = GebruikersFactory.GetGebruiker(Administrator);
-		
-		
+	public Lokaal getLokaalByNr(String nr) 
+	{
+		return lokalen.get(nr);
 	}
 	
 	
-	public void GetHuidigeIngelogd() {
+	public void fillStorage() 
+	{
+		// Declare strings
+		String Student = "Student";
+		String Docent = "Docent";
+		String Administrator = "Administrator";
 		
+		// Set Gebruikers in factory
+		Gebruiker kevin = gebruikersFactory.getGebruiker(Student);
+		Gebruiker martijn = gebruikersFactory.getGebruiker(Student);
+		Gebruiker wilco = gebruikersFactory.getGebruiker(Docent);
+		Gebruiker xing = gebruikersFactory.getGebruiker(Administrator);
+		
+		kevin.SetNaam("Kevin");
+		
+		martijn.SetNaam("Martijn");
+		
+		wilco.SetNaam("Wilco");
+		
+		xing.SetNaam("Xing");
+		
+		gebruikers.put("Kevin", kevin);
+		gebruikers.put("Martijn", martijn);
+		gebruikers.put("Wilco", wilco);
+		gebruikers.put("Xing", xing);
+	}
+	
+	// Set de huigige ingelogd
+	public void setHuidigeIngelogd(Gebruiker gebr) 
+	{
+		this.Gebruiker = gebr;
 	}
 	
 	
-	public void SetHuidigeIngelogd(Gebruiker gebr) {
-		
+	public Gebruiker GetHuidigeIngelogd() 
+	{
+		return Gebruiker;
 	}
 	
 	
-	public void GetLokaalByNr(String nr) {
-		
-	}
-	
-	
-	public void GetInstance() {
-		
+	public static Storage getInstance() 
+	{
+		if(instance == null) {
+			instance = new Storage();
+		}
+		return instance;
 	}
 }
