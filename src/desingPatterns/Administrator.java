65fc9cfec8d5c9328d63376f1b76685915532e70
Martijn <martjonk@hotmail.com>
2016-03-23 13:57:37 +0100

    Override op de methoden toegevoegd

--- src/desingPatterns/Administrator.java
+++ src/desingPatterns/Administrator.java
@@ -17,50 +17,59 @@ public class Administrator implements Gebruiker{
 	}
 
 	// Get naam
+	@Override
 	public String GetNaam()
 	{
 		return naam;
 	}
 	
 	// Set naam
+	@Override
 	public void SetNaam(String naam)
 	{
 		this.naam = naam;
 	}
 	
 	// Get all huidige reserveringen
+	@Override
 	public ArrayList<Reservering> GetHuidigeReservingen()
 	{
 		return huidigeReserveringen;
 	}
 	
 	// Add reservering to arraylist
+	@Override
 	public void AddHuidigeReservering(Reservering res)
 	{
 		huidigeReserveringen.add(res);
 	}
 	
 	// Remove huidige reservering TODO check of dit werkt zoals intended
+	@Override
 	public void RemoveHuidigeReservering(Reservering res)
 	{
 		huidigeReserveringen.remove(res);
 	}
 	
+	@Override
 	public int GetHuidigeReserveringsTijdInMin()
 	{
 		return huidigeReserveringsTijd;
 	}
 	
+	@Override
 	public void SetHuidigeReserveringsTijdInMin(int resTijd)
 	{
 		huidigeReserveringsTijd = resTijd;
 	}
 	
+	@Override
 	public int GetMaximaleReserveringsTijdInMin()
 	{
 		return maximaleReserveringstijdInMinuten;
 	}
 	
+	@Override
 	public int GetMaximaalAantalReserveringen()
 	{
 		return maximaalAantalReserveringen;
